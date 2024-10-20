e4193ec7fd74c81cc5c7af481999840bd99ecca7
fn78 <oh.seyy@yahoo.de>
2024-10-20 21:21:19 +0200

    WWGJ

--- code/scenes/mainmenu.js
+++ code/scenes/mainmenu.js
@@ -1,95 +1,66 @@
-var groundAnimation = 0;
+var coins = 0;
 
 scenes["mainmenu"] = new Scene(
     () => {
         // Init
+        // This whole pack gets executed every time the scene is loaded! Define objects here, variables only needed within this scene (-> this file), etc.
+
+        // This creates a square that covers the entire background
         createSquare("bg", 0, 0, 1, 1, "green");
 
-        createImage("menuground2", 0, 0.9, 1, 0.1, "menuground2");
+        // Display current page
+        // and add the var to the object (allows global access)
+        createText("currentPage", 0.5, 0.875, "", { size: 80 });
+        objects["currentPage"].page = 1;
 
-        createImage("menuground", 0, 0.8, 2, 0.1, "menuground");
-        createImage("menuground3", 0, 0, 2, 0.1, "menuground");
-
-        // by tpot
-        createImage("gameLogo", 0.5, 0.1, 0.4, 0.4, "gameLogo", { quadratic: true, centered: true });
-
-        createImage("logo", 0.5, 0, 0.4, 0.25, "logo", { quadratic: true, centered: true });
-        createText("version", 0.975, 0.8, "Version " + gameVersion, "black", 40, "right");
-
-        // Play button
-        createButton("playbutton", 0.3, 0.4, 0.4, 0.1, "button", () => {
-            game.stats.normalplays += 1;
-            game.stats.totalplays += 1;
-
-            pipes = [];
-            pipeSpawnTime = 1;
-            gameAcceleration = 1;
-            points = 0;
-            gameState = "running";
-            loadScene("play");
+        // Buttons to change page
+        createButton("button1", 0.1, 0.8, 0.2, 0.1, "#FFFFFF", () => {
+            if (objects["currentPage"].page > 1) objects["currentPage"].page--;
         });
-        createText("buttonText1", 0.5, 0.475, "Play", "black", 40);
-
-        // Stats button
-        createButton("playerbutton", 0.3, 0.525, 0.4, 0.1, "button", () => {
-            loadScene("player");
+        createButton("button2", 0.7, 0.8, 0.2, 0.1, "#FFFFFF", () => {
+            objects["currentPage"].page++;
         });
-        createText("buttonText2", 0.5, 0.6, "Player", "black", 40);
 
-        // Skins button
-        createButton("shopbutton", 0.3, 0.65, 0.4, 0.1, "button", () => {
-            loadScene("shop");
-        });
-        createText("buttonText3", 0.5, 0.725, "Shop", "black", 40);
+        // Text for the buttons (note: button text might become built-in soon)
+        // for text in buttons the general rule is x + (w / 2) and y + (h * 3/4)
+        createText("buttonText1", 0.1 + 0.2 / 2, 0.875, "Previous Page", { size: 40 });
+        createText("buttonText2", 0.7 + 0.2 / 2, 0.875, "Next Page", { size: 40 });
 
-        // Settings button
-        createButton("settingsbutton", 0.3, 0.775, 0.4, 0.1, "button", () => {
-            loadScene("settings");
-        });
-        createText("buttonText4", 0.5, 0.85, "Settings", "black", 40);
-        
-        // Left Icons
-        createButton("serverbutton", 0.02, 0.35, 0.08, 0.08, "whiteDiscord", () => {
-            window.open("https://discord.gg/CbBeJXKUrk");
-        }, { quadratic: true });
-        createText("wButtonText1", 0.06, 0.41, "Discord", "white", 32, "left");
+        // page content for examples
+        createSquare("mySquare1", 0.4, 0, 0.1, 0.1, "black");
+        createClickable("myClickable1", 0, 0, 1, 1, () => { console.log("Screen clicked!") });
+        createImage("myImage1", 0.4, 0.4, 0.2, 0.2, "placeholder", { quadratic: true });
+        createButton("myButton1", 0, 0, 0.2, 0.2, "#FFFFFF", () => { coins += 1 }, { quadratic: true });
+        createText("myText1", 0.5, 0.5, "Hello World", { size: 40 });
 
-        createButton("patchnotesbutton", 0.02, 0.45, 0.08, 0.08, "whiteNotes", () => {
-            loadScene("patchnotes");
-        }, { quadratic: true });
-        createText("wButtonText2", 0.06, 0.51, "Patch notes", "white", 32, "left");
-
-        createButton("websitebutton", 0.02, 0.55, 0.08, 0.08, "whiteWebsite", () => {
-            window.open("https://schrottii.github.io/");
-        }, { quadratic: true });
-        createText("wButtonText3", 0.06, 0.61, "Website", "white", 32, "left");
-
-        createButton("statsbutton", 0.02, 0.65, 0.08, 0.08, "whiteStats", () => {
-            loadScene("stats");
-        }, { quadratic: true });
-        createText("wButtonText4", 0.06, 0.71, "Stats", "white", 32, "left");
-
-        musicPlayer.src = "audio/menu-theme.mp3";
-        musicPlayer.volume = game.settings.music ? 1 : 0;
-        if (game.settings.music) musicPlayer.play();
+        // This sets the audio, reduces its volume and starts it
+        // (note: audio system will get reworked eventually)
+        wggjAudio.src = "audio/lofi-chill-rnb-logo-edit-201356.mp3";
+        wggjAudio.volume = 0.2; // I prefer having the volume a bit down
+        wggjAudio.play();
     },
     (tick) => {
         // Loop
-        groundAnimation += tick;
-        objects["menuground"].x -= tick;
-        objects["menuground3"].x -= tick;
-        if (groundAnimation >= 1) {
-            groundAnimation = 0;
-            objects["menuground"].x = 0;
-            objects["menuground3"].x = 0;
-        }
+        // The code here gets executed very frequently. Use this for animations, updating texts, movement, etc. - anything that isn't static
+        // Once you understand it, it's really easy and fun to use. I believe in you! :)
 
-        if (isMobile()) {
-            objects["version"].y = 0.975;
-            objects["wButtonText1"].y = 10;
-            objects["wButtonText2"].y = 10;
-            objects["wButtonText3"].y = 10;
-            objects["wButtonText4"].y = 10;
-        }
+        // Update current page display
+        let page = objects["currentPage"].page;
+        objects["currentPage"].text = "Page " + page;
+
+        if (page == 1) objects["mySquare1"].y = 0;
+        else objects["mySquare1"].y = 100;
+
+        if (page == 2) clickables["myClickable1"].y = 0;
+        else clickables["myClickable1"].y = 100;
+
+        if (page == 3) objects["myImage1"].power = true;
+        else objects["myImage1"].power = false;
+
+        if (page == 4) objects["myButton1"].power = true;
+        else objects["myButton1"].power = false;
+
+        if (page == 5) objects["myText1"].power = true;
+        else objects["myText1"].power = false;
     }
 );
